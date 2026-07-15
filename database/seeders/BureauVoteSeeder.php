0593efb5f9214a6309e13d8302f25ed0bd08db82
lalaina <lalainaarandriamiandrisoa@gmail.com>
2026-07-15 15:21:32 +0300

    saisi operateur

--- database/seeders/BureauVoteSeeder.php
+++ database/seeders/BureauVoteSeeder.php
@@ -11,9 +11,26 @@ class BureauVoteSeeder extends Seeder
     public function run(): void
     {
         $bureaux = [
-            ['code' => 'BV001', 'nom' => 'Bureau 1 - Salle principale'],
-            ['code' => 'BV002', 'nom' => 'Bureau 2 - Salle annexe'],
-            ['code' => 'BV003', 'nom' => 'Bureau 3 - Hall'],
+            ['code' => 'BV001', 'nom' => 'Bureau 1 - Salle principale1'],
+            ['code' => 'BV002', 'nom' => 'Bureau 2 - Salle principale2'],
+            ['code' => 'BV003', 'nom' => 'Bureau 3 - Salle principale3'],
+            ['code' => 'BV004', 'nom' => 'Bureau 4 - Salle principale4'],
+            ['code' => 'BV005', 'nom' => 'Bureau 5 - Salle principale5'],
+            ['code' => 'BV006', 'nom' => 'Bureau 6 - Salle principale6'],
+            ['code' => 'BV007', 'nom' => 'Bureau 7 - Salle principale7'],
+            ['code' => 'BV008', 'nom' => 'Bureau 8 - Salle principale8'],
+            ['code' => 'BV009', 'nom' => 'Bureau 9 - Salle principale9'],
+            ['code' => 'BV010', 'nom' => 'Bureau 10 - Salle principale10'],
+            ['code' => 'BV011', 'nom' => 'Bureau 11 - Salle principale11'],
+            // ['code' => 'BV012', 'nom' => 'Bureau 12 - Salle principale12'],
+            // ['code' => 'BV013', 'nom' => 'Bureau 13 - Salle principale13'],
+            // ['code' => 'BV014', 'nom' => 'Bureau 14 - Salle principale14'],
+            // ['code' => 'BV015', 'nom' => 'Bureau 15 - Salle principale15'],
+            // ['code' => 'BV016', 'nom' => 'Bureau 16 - Salle principale16'],
+            // ['code' => 'BV017', 'nom' => 'Bureau 17 - Salle principale17'],
+            // ['code' => 'BV018', 'nom' => 'Bureau 18 - Salle principale18'],
+            // ['code' => 'BV019', 'nom' => 'Bureau 19 - Salle principale19'],
+            // ['code' => 'BV020', 'nom' => 'Bureau 20 - Salle principale20'],
         ];
 
         foreach ($bureaux as $bv) {
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,7 +18,7 @@ class UserSeeder extends Seeder
             [
                 'name'           => 'Administrateur',
                 'email'          => 'admin@eglise.mg',
-                'password'       => Hash::make('password'),
+                'password'       => Hash::make('password987654321'),
                 'bureau_vote_id' => null,
             ]
         );
@@ -29,17 +29,28 @@ class UserSeeder extends Seeder
             ['name' => 'Opérateur BV001', 'email' => 'op1@eglise.mg', 'code' => 'BV001'],
             ['name' => 'Opérateur BV002', 'email' => 'op2@eglise.mg', 'code' => 'BV002'],
             ['name' => 'Opérateur BV003', 'email' => 'op3@eglise.mg', 'code' => 'BV003'],
+            ['name' => 'Opérateur BV004', 'email' => 'op4@eglise.mg', 'code' => 'BV004'],
+            ['name' => 'Opérateur BV005', 'email' => 'op5@eglise.mg', 'code' => 'BV005'],
+            ['name' => 'Opérateur BV006', 'email' => 'op6@eglise.mg', 'code' => 'BV006'],
+            ['name' => 'Opérateur BV007', 'email' => 'op7@eglise.mg', 'code' => 'BV007'],
+            ['name' => 'Opérateur BV008', 'email' => 'op8@eglise.mg', 'code' => 'BV008'],
+            ['name' => 'Opérateur BV009', 'email' => 'op9@eglise.mg', 'code' => 'BV009'],
+            ['name' => 'Opérateur BV0010', 'email' => 'op10@eglise.mg', 'code' => 'BV010'],
+            ['name' => 'Opérateur BV0011', 'email' => 'op11@eglise.mg', 'code' => 'BV011'],
+            // ['name' => 'Opérateur BV0012', 'email' => 'op12@eglise.mg', 'code' => 'BV012'],
+            // ['name' => 'Opérateur BV0013', 'email' => 'op13@eglise.mg', 'code' => 'BV013'],
+
         ];
 
         foreach ($operators as $op) {
             $bureau = BureauVote::where('code', $op['code'])->first();
-            
+
             $user = User::updateOrCreate(
                 ['email' => $op['email']],
                 [
                     'name'           => $op['name'],
                     'email'          => $op['email'],
-                    'password'       => Hash::make('password'),
+                    'password'       => Hash::make('password987654321'),
                     'bureau_vote_id' => $bureau->id,
                 ]
             );
